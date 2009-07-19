f09d237c2d7df5d8d22fae35857e8e12086521bb
Paul Infield-Harm <paul@infield-harm.org>
2009-07-18 20:53:17 -0400

    Figured out how to have GwtTestCase work

---
+++ test/mq/client/AddMovieFunctionalTest.java
@@ -0,0 +1,16 @@
+package mq.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class AddMovieFunctionalTest extends GWTTestCase {
+
+	@Override
+	public String getModuleName() {
+		return "mq.MQ";
+	}
+	
+	public void testSuccess() {
+		
+	}
+
+}
