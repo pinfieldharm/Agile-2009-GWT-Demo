ae9345cc831ea1be88cc34d5d89d7e1affce28c3
Paul Infield-Harm <paul@infield-harm.org>
2009-07-22 07:56:21 -0400

    Added suggestions to main page

--- src/mq/client/MQ.java
+++ src/mq/client/MQ.java
@@ -6,16 +6,16 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class MQ implements EntryPoint {
-	
+
 	/**
 	 * This is the entry point method.
 	 */
@@ -24,23 +24,30 @@ public class MQ implements EntryPoint {
 		final VerticalPanel mainPanel = new VerticalPanel();
 		mainPanel.setStylePrimaryName("mainPanel");
 		RootPanel.get().add(mainPanel);
-		
+
 		mainPanel.add(new HTML("<h1>Book Stack</h1>"));
-		
+
 		final HorizontalPanel inputPanel = new HorizontalPanel();
 		inputPanel.setStylePrimaryName("inputPanel");
 		mainPanel.add(inputPanel);
-		
-		final TextBox addBox = new TextBox();
+
+		MultiWordSuggestOracle oracle = new MultiWordSuggestOracle();
+		oracle.add("Cat Ballou");
+		oracle.add("Dog");
+		oracle.add("Horse");
+		oracle.add("Canary");
+
+		final SuggestBox addBox = new SuggestBox(oracle);
+
 		inputPanel.add(addBox);
 
 		final Button addButton = new Button("Add");
 		inputPanel.add(addButton);
-		
+
 		final VerticalPanel queuePanel = new VerticalPanel();
 		queuePanel.setStylePrimaryName("queuePanel");
 		mainPanel.add(queuePanel);
-		
+
 		addButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				String title = addBox.getText();
@@ -48,10 +55,10 @@ public class MQ implements EntryPoint {
 					queuePanel.insert(new MoviePanel(title, queuePanel), 0);
 					addBox.setText("");
 				}
-			}});
-		
-		queuePanel.add(new MoviePanel("Sample video", queuePanel));
-		queuePanel.add(new MoviePanel("Another", queuePanel));
-		
+			}
+		});
+
+		queuePanel.add(new MoviePanel("Agile Principles, Patterns, and Practices in C#", queuePanel));
+
 	}
 }
